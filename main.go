05443cb88f2319e70ea23320ce04620aec8af73f
Carlos S <carlos@aldrin.com>
2022-05-25 11:40:46 -0600

    Channels example

--- main.go
+++ main.go
@@ -3,32 +3,27 @@ package main
 import (
 	"fmt"
 	"time"
-	"sync"
 )
 
 func main() {
-	var wg sync.WaitGroup //Counter for waiting.
-	wg.Add(1)
+	c := make(chan string)
+	go count("sheep", c)
 
-	go func() {
-		count("sheep")
-		wg.Done() //Decrements counter by 1
-	}()
-
-	wg.Wait() // Wait until count is done.
-
-	//go count("sheep")
-	//go count("fish")
-	//time.Sleep(time.Millisecond * 200)
-
-	//fmt.Scanln()
+	for msg:= range c { 
+		//msg, open := <- c
+		// if !open {
+		// 	break
+		// }
+		fmt.Println(msg)
+	}
 }
 
-func count(thing string) {  //String channel to communicate to the main routine
+func count(thing string, c chan string) {  //String channel to communicate to the main routine
 	for i:= 1; i <= 5; i++ {		
-		fmt.Println(i, thing)
+		c <- thing		
 		time.Sleep(time.Millisecond * 500)
 	}
+	close(c)
 
 
 }
